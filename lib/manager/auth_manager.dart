c87d3f21efd7e49a8e4e84216496c7f5b6cade97
chicuongb2107180 <cuongb2107180@student.ctu.edu.vn>
2024-10-31 17:44:36 +0700

    cai dat dang nhap

---
+++ lib/manager/auth_manager.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
+import '../../models/user.dart';
+import '../../services/auth_service.dart';
+
+class AuthManager with ChangeNotifier {
+  late final AuthService _authService;
+  User? _loggedInUser;
+
+  AuthManager() {
+    _authService = AuthService(onAuthChange: (User? user) {
+      _loggedInUser = user;
+      notifyListeners();
+    });
+  }
+
+  bool get isAuth {
+    return _loggedInUser != null;
+  }
+
+  User? get user {
+    return _loggedInUser;
+  }
+
+  Future<User> signup(String user, String password) {
+    return _authService.signup(user, password);
+  }
+
+  Future<User> login(String username, String password) {
+    return _authService.login(username, password);
+  }
+
+  Future<void> tryAutoLogin() async {
+    final user = await _authService.getUserFromStore();
+    if (_loggedInUser != null) {
+      _loggedInUser = user;
+      notifyListeners();
+    }
+  }
+
+  Future<void> logout() async {
+    return _authService.logout();
+  }
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,78 @@
+import 'package:pocketbase/pocketbase.dart';
+
+import 'package:http/http.dart' as http;
+import '../models/user.dart';
+import './pocketbase_client.dart';
+
+class AuthService {
+    String _getAvatarImageUrl(PocketBase pb, RecordModel usermodel) {
+    final avatarImageName = usermodel.getStringValue('avatar');
+    return pb.files.getUrl(usermodel, avatarImageName).toString();
+  }
+
+  String _getCoverImageUrl(PocketBase pb, RecordModel usermodel) {
+    final coverImageName = usermodel.getStringValue('cover');
+    return pb.files.getUrl(usermodel, coverImageName).toString();
+  }
+  void Function(User? user)? onAuthChange;
+  AuthService({this.onAuthChange}) {
+    if (onAuthChange != null) {
+      getPocketBaseInstance().then((pb) {
+        pb.authStore.onChange.listen((event) {
+          onAuthChange!(event.model == null
+              ? null
+              : User.fromJson(event.model!.toJson()));
+        });
+      });
+    }
+  }
+  Future<User> signup(String username, String password) async {
+    final pb = await getPocketBaseInstance();
+    try {
+      final record = await pb.collection('users').create(body: {
+        'username': username,
+        'password': password,
+        'passwordConfirm': password,
+      });
+      return User.fromJson(record.toJson());
+    } catch (error) {
+      if (error is ClientException) {
+        throw Exception(error.response['message']);
+      }
+      throw Exception('An error occurred');
+    }
+  }
+
+  Future<User> login(String username, String password) async {
+    final pb = await getPocketBaseInstance();
+    try {
+      print('username: $username');
+      final authRecord =
+          await pb.collection('users').authWithPassword(username, password);
+        print('authRecord: $authRecord');
+      return User.fromJson(authRecord.record!.toJson()..addAll({
+        'url_avatar': _getAvatarImageUrl(pb, authRecord.record!),
+        'url_cover': _getCoverImageUrl(pb, authRecord.record!),
+      }));
+    } catch (error) {
+      if (error is ClientException) {
+        throw Exception(error.response['message']);
+      }
+      throw Exception('An error occurred');
+    }
+  }
+
+  Future<void> logout() async {
+    final pb = await getPocketBaseInstance();
+    pb.authStore.clear();
+  }
+
+  Future<User?> getUserFromStore() async {
+    final pb = await getPocketBaseInstance();
+    final model = pb.authStore.model;
+    if (model == null) {
+      return null;
+    }
+    return User.fromJson(model.toJson());
+  }
+}
